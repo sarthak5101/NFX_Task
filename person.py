548a1e593e34b3ce5b898ebd1ed138f607783b78
Sarthak Sharma <89703198+sarthak5101@users.noreply.github.com>
2023-08-28 08:49:03 +0530

    person.py

--- person.py
+++ person.py
@@ -1,69 +1,47 @@
 import cv2
 import numpy as np
+import tensorflow as tf
 
-net = cv2.dnn.readNet('yolov3.weights', 'yolov3.cfg')
+# Load the pre-trained model
+model = tf.saved_model.load('saved_model_dir/saved_model')
 
-classes = []
-with open('coco.names', 'r') as f:
+
+# Load COCO class labels
+classes = {}
+with open('coco_labels.txt', 'r') as f:
     classes = f.read().splitlines()
 
+# Open the video file
 video_path = 'video.mp4'
 cap = cv2.VideoCapture(video_path)
 
-# Adjust these parameters for optimization
-resize_factor = 0.5  # Resize factor for input frames
-confidence_threshold = 0.5  # Minimum confidence for detections
-frame_skip = 3  # Process every nth frame
-
 while cap.isOpened():
-    for _ in range(frame_skip):
-        ret, frame = cap.read()
-        if not ret:
-            break
-    
+    ret, frame = cap.read()
     if not ret:
         break
 
-    height, width, _ = frame.shape
-    resized_frame = cv2.resize(frame, None, fx=resize_factor, fy=resize_factor)
+    input_image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    input_tensor = tf.convert_to_tensor(input_image)
+    input_tensor = input_tensor[tf.newaxis, ...]
 
-    blob = cv2.dnn.blobFromImage(resized_frame, scalefactor=1/255, size=(320, 320), swapRB=True, crop=False)
+    detections = model(input_tensor)
 
-    net.setInput(blob)
-    layer_outputs = net.forward(net.getUnconnectedOutLayersNames())
+    for detection in detections['detection_boxes'][0]:
+        if detection[2] > 0.5:  # Confidence threshold
+            y_min, x_min, y_max, x_max = detection
+            y_min = int(y_min * frame.shape[0])
+            x_min = int(x_min * frame.shape[1])
+            y_max = int(y_max * frame.shape[0])
+            x_max = int(x_max * frame.shape[1])
+            
+            class_id = int(detection[1])
+            label = classes[class_id]
 
-    class_ids = []
-    confidences = []
-    boxes = []
+            color = (0, 255, 0)  # Green
+            cv2.rectangle(frame, (x_min, y_min), (x_max, y_max), color, 2)
+            cv2.putText(frame, label, (x_min, y_min - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
 
-    for output in layer_outputs:
-        for detection in output:
-            scores = detection[5:]
-            class_id = np.argmax(scores)
-            confidence = scores[class_id]
-            if confidence > confidence_threshold:
-                center_x = int(detection[0] * width)
-                center_y = int(detection[1] * height)
-                w = int(detection[2] * width)
-                h = int(detection[3] * height)
-                x = int(center_x - w / 2)
-                y = int(center_y - h / 2)
-
-                boxes.append([x, y, w, h])
-                confidences.append(float(confidence))
-                class_ids.append(class_id)
-
-    indexes = cv2.dnn.NMSBoxes(boxes, confidences, confidence_threshold, 0.4)
-
-    for i in indexes:
-        x, y, w, h = boxes[i]
-        label = str(classes[class_ids[i]])
-        confidence = str(round(confidences[i], 2))
-        color = (0, 255, 0)
-        cv2.rectangle(frame, (x, y), (x + w, y + h), color, 2)
-        cv2.putText(frame, f'{label} {confidence}', (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
-
-    cv2.imshow('Person Detection', frame)
+    cv2.imshow('Object Detection', frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
